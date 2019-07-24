7697ad6afc7f5605be5ae15f5a03746c4237420d
Nestor Moschen <nestor.easysoftware@gmail.com>
2019-07-24 14:12:17 -0300

    New methods added

--- src/mock-api/mock-api.ts
+++ src/mock-api/mock-api.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpEventType } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
@@ -14,10 +14,8 @@ export class MockApi implements HttpInterceptor {
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-		const url = req.url;
-
-		return this.isApiRequest(url)
-			? this.getResponse(req.method, url).pipe(delay(1500))
+		return this.isApiRequest(req.url)
+			? this.getResponse(req).pipe(delay(1000))
 			: next.handle(req);
 	}
 
@@ -26,7 +24,11 @@ export class MockApi implements HttpInterceptor {
 		return baseUrl === 'mock-api';
 	}
 
-	private getResponse(method: string, url: string): Observable<HttpResponse<any>> {
+	private getResponse(req: HttpRequest<any>): Observable<HttpResponse<any>> {
+		const url = req.url;
+		const method = req.method;
+		const body = req.body;
+
 		const splittedUrl = url.split('/');
 		const resource = splittedUrl[1];
 		const id = splittedUrl[2];
@@ -34,16 +36,31 @@ export class MockApi implements HttpInterceptor {
 		return new Observable<HttpResponse<any>>(observer => {
 			switch (resource) {
 				case 'languages':
-					const response = new HttpResponse({ body: this.getResponseForLanguages(method, id), status: 200 });
-					observer.next(response);
+
+					const responseForLanguages = this.getResponseForLanguages(method, id, body);
+
+					const hasError = responseForLanguages instanceof Error;
+					if (hasError) {
+						observer.error((responseForLanguages as Error).message);
+						return;
+					}
+
+					observer.next(new HttpResponse({ body: responseForLanguages, status: 200 }));
+					observer.complete();
 			}
 		});
 	}
 
-	private getResponseForLanguages(method: string, id: string): any {
+	private getResponseForLanguages(method: string, id: string, data?: any): any {
 		switch (method) {
 			case 'GET':
 				return !id ? this.getAllLanguages() : this.getOneLanguage(id);
+			case 'POST':
+				return this.createLanguage(data);
+			case 'PUT':
+				return this.updateLanguage(id, data);
+			case 'DELETE':
+				return this.deleteLanguage(id);
 		}
 	}
 
@@ -53,5 +70,44 @@ export class MockApi implements HttpInterceptor {
 
 	private getOneLanguage(id: string): any {
 		return this.languages.find(language => language.id === +id);
+	}
+
+	private createLanguage(data: any): Error | void {
+
+		const { id, name, creationDate, popularity } = data;
+
+		if (id || !name || !creationDate || !popularity) {
+			return new Error('Los parámetros name, creationDate, popularity y id son requeridos');
+		}
+
+		const lastId = this.languages.reduce((higherId, l) => higherId < l.id ? l.id : higherId, this.languages[0].id);
+		const language = { id: lastId + 1, name, creationDate, popularity };
+
+		this.languages.push(language);
+	}
+
+	private updateLanguage(id: string, data: any): Error | void {
+
+		const { name, creationDate, popularity } = data;
+		if (!name || !creationDate || !popularity) {
+			return new Error('Los parámetros name, creationDate y popularity son requeridos');
+		}
+
+		const index = this.languages.findIndex(l => l.id === +id);
+		if (index === -1) {
+			return new Error('El lenguaje que se intenta actualizar no existe');
+		}
+
+		this.languages[index] = { ...this.languages[index], name, creationDate, popularity };
+	}
+
+	private deleteLanguage(id: string): Error | void {
+
+		const index = this.languages.findIndex(l => l.id === +id);
+		if (index === -1) {
+			return new Error('El lenguaje que se intenta borrar no existe');
+		}
+
+		this.languages = this.languages.filter(language => language.id !== +id);
 	}
 }
