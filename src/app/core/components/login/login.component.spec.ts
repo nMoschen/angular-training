159dc6c6d5a457638c46ed9274055bc5d2204190
Nestor Moschen <nestor.easysoftware@gmail.com>
2019-08-25 18:13:25 -0300

    Clase 6

--- src/app/core/components/login/login.component.spec.ts
+++ src/app/core/components/login/login.component.spec.ts
@@ -4,6 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '../../services/auth.service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { AuthServiceStub } from '../../../../testing/auth-service-stub';
 
 describe('LoginComponent', () => {
 	let component: LoginComponent;
@@ -18,7 +22,7 @@ describe('LoginComponent', () => {
 			],
 			declarations: [LoginComponent],
 			providers: [
-				{ provide: AuthService, useValue: {} }
+				{ provide: AuthService, useClass: AuthServiceStub }
 			]
 		})
 			.compileComponents();
@@ -32,5 +36,78 @@ describe('LoginComponent', () => {
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	it('debe validar la deteccion de cambios', () => {
+		const passwordInput: HTMLInputElement = fixture.debugElement.query(By.css('[formControlName=password]')).nativeElement;
+		component.hide = false;
+		fixture.detectChanges();
+		expect(passwordInput.type).toBe('text');
+	});
+
+	it('debe loguear un usuario con credenciales validas', () => {
+
+		const emailInput: HTMLInputElement = fixture.debugElement.query(By.css('[formControlName=email]')).nativeElement;
+		const passwordInput: HTMLInputElement = fixture.debugElement.query(By.css('[formControlName=password]')).nativeElement;
+		const loginButton: HTMLButtonElement = fixture.debugElement.query(By.css('[type=submit]')).nativeElement;
+
+		const router: Router = TestBed.get(Router);
+		const navigateSpy = spyOn(router, 'navigate');
+
+		spyOn(TestBed.get(AuthService), 'login').and.returnValue(of(true));
+
+		const fakeEmail = 'fake@email.com';
+		const fakePassword = 'fake-pass-1234';
+
+		emailInput.value = fakeEmail;
+		emailInput.dispatchEvent(new Event('input'));
+
+		passwordInput.value = fakePassword;
+		passwordInput.dispatchEvent(new Event('input'));
+
+		loginButton.click();
+
+		// Mi expectativa es navegar a '/'
+		expect(navigateSpy).toHaveBeenCalledWith(['/']);
+	});
+
+	it('debe loguear un usuario con credenciales validas (sin usar el DOM)', () => {
+
+		const navigateSpy = spyOn(TestBed.get(Router), 'navigate');
+		spyOn(TestBed.get(AuthService), 'login').and.returnValue(of(true));
+
+		const fakeEmail = 'fake@email.com';
+		const fakePassword = 'fake-pass-1234';
+
+		component.form.setValue({
+			email: fakeEmail,
+			password: fakePassword
+		});
+
+		component.login();
+
+		expect(navigateSpy).toHaveBeenCalledWith(['/']);
+	});
+
+	it('no debe loguear un usuario con credenciales incompletas', () => {
+
+		const emailInput: HTMLInputElement = fixture.debugElement.query(By.css('[formControlName=email]')).nativeElement;
+		const loginButton: HTMLButtonElement = fixture.debugElement.query(By.css('[type=submit]')).nativeElement;
+
+		const router: Router = TestBed.get(Router);
+		const navigateSpy = spyOn(router, 'navigate');
+
+		spyOn(TestBed.get(AuthService), 'login').and.returnValue(of(true));
+
+		const fakeEmail = 'fake@email.com';
+
+		emailInput.value = fakeEmail;
+		emailInput.dispatchEvent(new Event('input'));
+
+		loginButton.click();
+
+		// Mi expectativa es navegar a '/'
+		expect(navigateSpy).not.toHaveBeenCalledWith(['/']);
+
 	});
 });
--- src/app/core/services/api.service.spec.ts
+++ src/app/core/services/api.service.spec.ts
@@ -1,20 +1,104 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
 import { LoaderService } from '../loader/loader.service';
+import { environment } from '../../../environments/environment';
+import { HttpErrorResponse } from '@angular/common/http';
+
+class LoaderServiceStub {
+	showLoader() { }
+	hideLoader() { }
+}
 
 describe('ApiService', () => {
+
+	const baseUrl = environment.api.base;
+	let apiService: ApiService;
+	let loaderService: LoaderService;
+	let httpTestingController: HttpTestingController;
+
 	beforeEach(() => TestBed.configureTestingModule({
 		imports: [
 			HttpClientTestingModule
 		],
 		providers: [
-			{ provide: LoaderService, useValue: {} }
+			{ provide: LoaderService, useClass: LoaderServiceStub }
 		]
 	}));
 
+	beforeEach(() => {
+		apiService = TestBed.get(ApiService);
+		loaderService = TestBed.get(LoaderService);
+		httpTestingController = TestBed.get(HttpTestingController);
+	});
+
+	afterEach(() => {
+		httpTestingController.verify();
+	});
+
 	it('should be created', () => {
-		const service: ApiService = TestBed.get(ApiService);
-		expect(service).toBeTruthy();
+		expect(apiService).toBeTruthy();
+	});
+
+	it('debe crear un recurso', () => {
+
+		const fakeResource = 'fake-resource';
+		const fakeData = { data: 'fake-data' };
+		const fakeUrl = `${baseUrl}/${fakeResource}`;
+
+		const showLoaderSpy = spyOn(loaderService, 'showLoader');
+		const hideLoaderSpy = spyOn(loaderService, 'hideLoader');
+
+		apiService
+			.create(fakeResource, fakeData)
+			.subscribe((resp) => {
+				expect(resp.data.id).toBe(1);
+				expect(resp.data.name).toBe('fake-created-name');
+				// 3. Tiene que esconderse el loader al finalizar la llamada a la API
+				expect(hideLoaderSpy).toHaveBeenCalledTimes(1);
+			});
+
+		// 1. Tiene que mostrarse el loader al iniciar la llamada a la API
+		expect(showLoaderSpy).toHaveBeenCalledTimes(1);
+		// 2. Tiene que realizar una llamada POST a la URL del recurso y con cierta data
+		const req = httpTestingController.expectOne(fakeUrl);
+		expect(req.request.method).toBe('POST');
+
+		req.flush({ data: { id: 1, name: 'fake-created-name' } }, { status: 201, statusText: 'Created' });
+	});
+
+	it('debe manejar un error cuando un recurso no es creado', () => {
+
+		const fakeResource = 'fake-resource';
+		const fakeData = { data: 'fake-data' };
+		const fakeUrl = `${baseUrl}/${fakeResource}`;
+
+		const showLoaderSpy = spyOn(loaderService, 'showLoader');
+		const hideLoaderSpy = spyOn(loaderService, 'hideLoader');
+
+		const alertSpy = spyOn(window, 'alert');
+
+		apiService
+			.create(fakeResource, fakeData)
+			.subscribe(
+				() => { },
+				(error: HttpErrorResponse) => {
+					// 3. Tiene que escoderse el loader al finalizar la llamada a la API
+					expect(hideLoaderSpy).toHaveBeenCalledTimes(1);
+					// 4. Tiene que mostrarse un alert
+					expect(alertSpy).toHaveBeenCalledTimes(1);
+					// 5. El status y statusText sean 500 e 'Internal Server Error'
+					expect(error.status).toBe(500);
+					expect(error.statusText).toBe('Internal Server Error');
+				}
+			);
+
+		// 1. Tiene que mostrarse el loader al iniciar la llamada a la API
+		expect(showLoaderSpy).toHaveBeenCalledTimes(1);
+		// 2. Tiene que realizar una llamada POST a la URL del recurso y con cierta data
+		const req = httpTestingController.expectOne(fakeUrl);
+		expect(req.request.method).toBe('POST');
+
+		req.flush({}, { status: 500, statusText: 'Internal Server Error' });
 	});
 });
